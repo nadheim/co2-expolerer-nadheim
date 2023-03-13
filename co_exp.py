425557495cd5173901d167e443266d3057a5bf9e
nadheim <113419401+nadheim@users.noreply.github.com>
2023-03-13 10:59:37 +0100

    Update co_exp.py

--- co_exp.py
+++ co_exp.py
@@ -21,6 +21,41 @@ from dash_bootstrap_templates import load_figure_template
 dbc_css = 'https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.2/dbc.min.css'
 load_figure_template('bootstrap')
 
+# Read dataframe
+df = pd.read_csv('world_1960_2021.csv')
+
+df_update = df[df.year > 1990]
+
+# Set year as index
+df_update.set_index('year', inplace = True)
+
+
+fig_kt = px.line(
+    df_update, 
+    y = 'EN.ATM.CO2E.KT',                                
+)
+
+fig_kt.update_layout(
+    yaxis_title = None,                          
+    xaxis_title = None,                         
+    title = 'CO2 emmisions (kt)',  
+    title_x = 0.5,   
+    margin = {'l' : 0, 'r' : 0}
+)
+
+fig_pc = px.line(
+    df_update, 
+    y = 'EN.ATM.CO2E.PC',                                
+)
+
+fig_pc.update_layout(
+    yaxis_title = None,                          
+    xaxis_title = None,                         
+    title = 'CO2 emissions (metric tons per capita)',  
+    title_x = 0.5,   
+    margin = {'l' : 0, 'r' : 0}
+)
+
 # Create application
 app = Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP, dbc_css])
 server = app.server
@@ -35,6 +70,18 @@ app.layout = dbc.Container(
                [World Development Indicators](https://datatopics.worldbank.org/world-development-indicators/) 
                database."""
         ),
+        
+        # Place plots side by side
+        dbc.Row(
+            children = [
+            
+            # Column 1: price plot
+            dbc.Col(dcc.Graph(figure = fig_kt), width = 6),
+            
+            # Column 2: volume plot
+            dbc.Col(dcc.Graph(figure = fig_pc), width = 6)
+            ]
+        )  
         
     ],
     className = 'dbc'
